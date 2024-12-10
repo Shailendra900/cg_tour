7cc8cf7f6ac1db38f57f6710460c6cc029c980bb
Shailendra Verma <shailkumarverma2017@gmail.com>
2024-12-10 18:33:14 +0530

    first

---
+++ docs/server/chunk-3OI6M4FK.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a as e,b as r,c as p,d as i,e as n,j as m,m as a,o as f,p as s,s as c,y as d}from"./chunk-X53DNMB2.mjs";var u=(()=>{class o{constructor(){this.title="CG_Tour"}static{this.\u0275fac=function(t){return new(t||o)}}static{this.\u0275cmp=e({type:o,selectors:[["app-root"]],standalone:!0,features:[n],decls:2,vars:0,template:function(t,A){t&1&&(r(0,"h3"),i(1," Cg_Tour page"),p())}})}}return o})();var l=[];var g={providers:[m({eventCoalescing:!0}),d(l),s()]};var x={providers:[c()]},C=a(g,x);var h=()=>f(u,C),j=h;export{j as a};
---
+++ docs/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-3OI6M4FK.mjs";import"./chunk-X53DNMB2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
